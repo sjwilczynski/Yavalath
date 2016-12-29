e256f81cf2e22eade391bd7d0da933d89615605f
Jakub Schreiber <schreiber.jakub@wp.pl>
2016-12-29 19:15:23 +0100

    test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function(request, response){
     response.writeHead(200, {"contentType" : "text/plain"})
-    response.end("yo\n")
+    response.end("hello\n")
 
 
 }).listen(process.env.PORT)
