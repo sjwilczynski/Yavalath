529c163c64fb0a20bddf96a705cdef3b720b1770
Jakub Schreiber <schreiber.jakub@wp.pl>
2016-12-30 01:54:37 +0100

    game base

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var http = require('http');
 var socket = require('socket.io');
 var fs = require('fs');
 var express = require('express');
+var phaser = require('phaser');
 
 var html = fs.readFileSync('app.html', 'utf-8');
 
@@ -32,5 +33,22 @@ setInterval( function() {
     var date = new Date().toString();
     io.emit( 'message', date.toString() );
 }, 1000 );
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload() {
+
+    game.load.image('sky', 'assets/sky.png');
+    game.load.image('ground', 'assets/platform.png');
+    game.load.image('star', 'assets/star.png');
+    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+}
+
+function create() {
+}
+
+function update() {
+}
+game.add.sprite(0, 0, 'star');
 
 console.log( 'server listens' );
